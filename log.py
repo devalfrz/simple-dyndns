ec78a4816febe8fcb4f90551ddc246c5c8e23c13
Alfredo Rius <alfredo.rius@gmail.com>
2016-11-18 16:31:10 -0600

    Restart browser if connection fails

---
+++ log.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env
+
+"""
+Creates log file
+"""
+from datetime import datetime
+import sys, os
+
+class Log:
+    def __init__(self,log_file='/var/log/simpledyndns/error.log'):
+        self.log_file = log_file
+
+    def write(self,message):
+            """
+            Write in log file
+            """
+
+            # Get time string
+            now = datetime.now().strftime("%a %b %d %H:%M:%S.%f")
+
+            try:
+                # Create a log directory
+                os.makedirs(os.path.dirname(self.log_file))
+            except OSError:
+                pass
+
+            # Append to log
+
+            f = open(self.log_file,'a')
+            f.write("[%s] %s\n" % (now,message))
+            f.close()
+
+
+
