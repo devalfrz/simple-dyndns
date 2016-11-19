514ef4de17c24842e59dc4b37130dc81040980de
Alfredo Rius <alfredo.rius@gmail.com>
2016-11-18 20:42:27 -0600

    Renamed log file

--- log.py
+++ log.py
@@ -7,7 +7,7 @@ from datetime import datetime
 import sys, os
 
 class Log:
-    def __init__(self,log_file='/var/log/simpledyndns/error.log'):
+    def __init__(self,log_file='/var/log/simpledyndns/simpledyndns.log'):
         self.log_file = log_file
 
     def write(self,message):
